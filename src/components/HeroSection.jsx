cf38538a049dec880b286e59373f6d7da635b3ba
Usiel-03A7 <shebastyan@gmail.com>
2025-09-20 15:25:22 -0600

    Remove name

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -9,9 +9,6 @@ export default function HeroSection() {
         className="w-full h-full  object-cover"
       />
       <div className="absolute inset-0 bg-black/50 flex flex-col items-center justify-center text-center px-6">
-        <h2 className="text-4xl md:text-6xl font-extrabold text-[#FECD5F]">
-          Nutrimikee
-        </h2>
         <p className="mt-4 text-lg md:text-2xl text-white italic">
           Only healthy vibes
         </p>
